df7060121ea1af6df0c83b716a0d7831da99b793
Vinod83GH <vinod.gangwar@galepartners.com>
2022-05-17 16:24:04 +0530

    added changes

--- EmailTemplateValidator/popup.js
+++ EmailTemplateValidator/popup.js
@@ -6,7 +6,6 @@ var htmlAttributeText = document.getElementById("htmlAttributes");
 var saveDOM = document.getElementById("saveDOM");
 var linkValidator = document.getElementById("btnValidateLink");
 var figmaPullContent = document.getElementById("btnPullContent");
-var validateMatchOptCtrl = document.getElementById("match_options");
 
 figmaPullContent.addEventListener("click", async () => {
   var figmaFile = document.getElementById("txtFigmaFile");
@@ -545,92 +544,207 @@ function GetDOMStructureFromCurrentTab(ctrls) {
   hiddenElement.click();
 }
 
-function ValidateNode(sourceFileData, ctrls, ignoreNestedCtrl) {
+function ValidateNode(sourceFileData, ctrls, ignoreNestedCtrl, validateOptionsCtrlValue) {
   if (!sourceFileData) {
     return;
   }
+
+  // clearPreviousErrors();
   //Clear previous error message by removing the class
   var errorCtrls = document.querySelectorAll(".gale-validation-error-box");
   for (y = 0; y < errorCtrls.length; y++) {
     errorCtrls[y].classList.remove("gale-validation-error-box");
   }
-
-  let allRows = sourceFileData.split(/\r?\n|\r/);
+  
+  console.log(`Validation by - ${validateOptionsCtrlValue}`);
+  if (validateOptionsCtrlValue=='id'){
+    var errorCtrls=[];
+    let allRows = sourceFileData.split(/\r?\n|\r/);
+  
   //Filter data
-  let filteredSourceRow = [];
-  for (x = 1; x < allRows.length; x++) {
-    let splited = allRows[x].split(",");
-    if (
-      ctrls.find((element) => element.toUpperCase() == splited[0].toUpperCase())
-    ) {
-      filteredSourceRow.push(splited);
+  // console.log(CSV_COLUMNS);
+    let filteredSourceRow = [];
+    for (x = 1; x < allRows.length; x++) {
+      let splited = allRows[x].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/)
+      // console.log(splited);
+      if (splited[1]){
+        filteredSourceRow.push(splited);
+      }
+    }
+  
+    for (i = 0; i < filteredSourceRow.length; i++) {
+      // console.log(filteredSourceRow[i])
+      var ctrlId_Csv= filteredSourceRow[i][1];
+      var ctrlText_Csv= filteredSourceRow[i][2].replaceAll('"', '');
+      var controlDoc=document.getElementById(ctrlId_Csv);
+      if (!controlDoc){
+        continue
+      }
+
+      ctrlName = controlDoc.tagName;
+      ctrlId = controlDoc.getAttribute("Id");
+      
+      ctlvalue = controlDoc.outerText;
+      ctlvalue = ctlvalue.replaceAll('"', '');
+      ctlvalue = ctlvalue.replace(/\u00a0/g, " ");
+      ctlvalue = ctlvalue.replace(/\u200C/g, '');
+      ctlvalue = ctlvalue.trim();
+
+      var source= "";
+      if (ctrlName === "A") {
+        source = controlDoc.getAttribute("href");
+      } else if (ctrlName === "IMG") {
+        source = controlDoc.getAttribute("src");
+      }
+      if (source === "#" || source === null) {
+        source = "";
+      }
+
+      let altText = controlDoc.getAttribute("alt");
+      if (
+        altText === undefined ||
+        altText === null
+      ) {
+        altText = "";
+      }
+
+      combinedData = ctrlName + "," + ctrlId + "," + ctlvalue + "," + source + "," + altText;
+
+      filteredSourceRow[i][2] = filteredSourceRow[i][2].replaceAll('"', '');
+      var combinedCsvRow = filteredSourceRow[i].join(",");
+
+      // console.log(ctrlText_Csv);
+      if (combinedData!==combinedCsvRow){
+        console.log(`Mismatch content:> Row No - ${i}---> \n ${combinedCsvRow}`);
+        controlDoc.classList.add("gale-validation-error-box");
+        errorCtrls.push(controlDoc);
+      }
     }
   }
+  else{
+    let allRows = sourceFileData.split(/\r?\n|\r/);
+    //Filter data
+    let filteredSourceRow = [];
+    for (x = 1; x < allRows.length; x++) {
+      let splited = allRows[x].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+      // console.log(splited);
+      if (splited.length==5 &&
+        ctrls.find((element) => element.toUpperCase() == splited[0].toUpperCase())
+      ) {
+        // console.log(splited);
+        filteredSourceRow.push(splited);
+      }
 
-  var nodes = document.querySelectorAll(ctrls.join(","));
-  var filteredNode = [];
-  for (i = 0; i < nodes.length; i++) {
-    if (ignoreNestedCtrl) {
-      const parentNode = nodes[i].parentNode.nodeName;
-      if (!parentNode.startsWith("H") && !parentNode.startsWith("P")) {
+    }
+  
+    var nodes = document.querySelectorAll(ctrls.join(","));
+    var filteredNode = [];
+    for (i = 0; i < nodes.length; i++) {
+      if (ignoreNestedCtrl) {
+        const parentNode = nodes[i].parentNode.nodeName;
+        if (!parentNode.startsWith("H") && !parentNode.startsWith("P")) {
+          filteredNode.push(nodes[i]);
+        }
+      } else {
         filteredNode.push(nodes[i]);
       }
-    } else {
-      filteredNode.push(nodes[i]);
     }
-  }
-  if (filteredSourceRow.length !== nodes.length) {
-    alert(
-      "Source column count(" +
-        filteredSourceRow.length +
-        ") and page control count(" +
-        nodes.length +
-        ") does not match."
-    );
-    return;
-  }
-  var errorCtrls = [];
-  for (i = 0; i < filteredSourceRow.length; i++) {
-    var nodeName = filteredNode[i].nodeName;
-    var source = null;
+    if (filteredSourceRow.length !== nodes.length) {
+      alert(
+        "Source column count(" +
+          filteredSourceRow.length +
+          ") and page control count(" +
+          nodes.length +
+          ") does not match."
+      );
+      return;
+    }
 
-    if (nodeName === "A") {
-      source = nodes[i].getAttribute("href");
-    } else if (nodeName === "IMG") {
-      source = nodes[i].getAttribute("src");
-    }
-    if (source === "#" || source === null) {
-      source = "";
-    }
-    let ctrlText = filteredNode[i].outerText;
-    if (
-      filteredNode[i].outerText === undefined ||
-      filteredNode[i].outerText === null
-    ) {
-      ctrlText = "";
-    }
-    let altText = filteredNode[i].getAttribute("alt");
-    if (
-      filteredNode[i].getAttribute("alt") === undefined ||
-      filteredNode[i].getAttribute("alt") === null
-    ) {
-      altText = "";
-    }
-    let compareString =
-      nodeName + "," + ctrlText + "," + source + "," + altText;
+    var errorCtrls = [];
+    for (i = 0; i < filteredSourceRow.length; i++) {
+      var nodeName = filteredNode[i].nodeName;
+      var source = null;
+  
+      if (nodeName === "A") {
+        source = nodes[i].getAttribute("href");
+      } else if (nodeName === "IMG") {
+        source = nodes[i].getAttribute("src");
+      }
+      if (source === "#" || source === null) {
+        source = "";
+      }
+      let ctlId = filteredNode[i].getAttribute("Id");
+      if (
+        ctlId === undefined ||
+        ctlId === null
+      ) {
+        ctlId = "";
+      }
+      
+      let ctrlText = filteredNode[i].innerText;
+      if (
+        filteredNode[i].innerText === undefined ||
+        filteredNode[i].innerText === null
+      ) {
+        ctrlText = "";
+      }
+      else{
+        ctrlText = ctrlText.replaceAll('"', '');
+        ctrlText = ctrlText.replace(/\u00a0/g, " ");
+        ctrlText = ctrlText.replace(/\u200C/g, '');
+        ctrlText = ctrlText.trim();
+      }
 
-    if (filteredSourceRow[i].join(",") !== compareString) {
-      filteredNode[i].classList.add("gale-validation-error-box");
-      errorCtrls.push(filteredNode[i]);
+      let altText = filteredNode[i].getAttribute("alt");
+      if (
+        filteredNode[i].getAttribute("alt") === undefined ||
+        filteredNode[i].getAttribute("alt") === null
+      ) {
+        altText = "";
+      }
+      
+      let compareString =
+      nodeName + "," + ctlId + "," + ctrlText + "," + source + "," + altText;
+
+      // Replace if any double quotes present on value text
+      filteredSourceRow[i][2] = filteredSourceRow[i][2].replaceAll('"', '');
+      var combinedCsvRow = filteredSourceRow[i].join(",");
+  
+      if (combinedCsvRow !== compareString) {
+        console.log(`Mismatch content:> Row No - ${i}---> \n`)
+        console.log(combinedCsvRow);
+        filteredNode[i].classList.add("gale-validation-error-box");
+        errorCtrls.push(filteredNode[i]);
+      }
+
+      // var csvText = filteredSourceRow[i][2].replaceAll('"', '');
+
+      // if (csvText !== ctrlText) {
+      //   console.log(filteredNode[i]);
+      //   console.log(ctrlText);
+      //   console.log(csvText);
+      //   filteredNode[i].classList.add("gale-validation-error-box");
+      //   errorCtrls.push(filteredNode[i]);
+      // }
+
     }
   }
+  
+
+  // //Clear previous error message by removing the class
+  // var errorCtrls = document.querySelectorAll(".gale-validation-error-box");
+  // for (y = 0; y < errorCtrls.length; y++) {
+  //   errorCtrls[y].classList.remove("gale-validation-error-box");
+  // }
+  
+
   if (errorCtrls.length === 0) {
     alert("Validation successful. No discrepancy detected!!!");
   } else {
     alert(
       "Validation error. Please check console log for list of controls which has issue"
     );
-    console.log(errorCtrls);
+    // console.log(errorCtrls);
     // create a style element
     const style = document.createElement("style");
 
@@ -652,6 +766,9 @@ function ValidateNode(sourceFileData, ctrls, ignoreNestedCtrl) {
 }
 
 validateTag.addEventListener("click", async () => {
+  // console.log(CSV_COLUMNS);
+  validateOptionsCtrl = document.getElementById("match_options"); // validate options control
+  // console.log(validateOptionsCtrl.value);
   var srcFile = document.getElementById("src-lcaol-file");
   var ctrlToValidate = document.querySelectorAll(".ctrl-chkbox:checked");
   var ignoreNestedCtrl = document.getElementById(
@@ -677,7 +794,7 @@ validateTag.addEventListener("click", async () => {
         chrome.scripting.executeScript({
           target: { tabId: tab.id },
           func: ValidateNode,
-          args: [e.target.result, seletedCtrls, ignoreNestedCtrl],
+          args: [e.target.result, seletedCtrls, ignoreNestedCtrl, validateOptionsCtrl.value],
         });
       });
       reader.readAsText(sourceFile, "UTF-8");
